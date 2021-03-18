9033c54856b0823f58c5076518bd201b5f7b674e
Starlight Romero <starlightromero@protonmail.com>
2021-03-17 23:48:52 -0700

    [update] prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  trailingComma: 'es5',
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
-  bracketSpacing: true
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
 }
