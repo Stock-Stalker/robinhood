0e54cb4d51eaa7c4fdc16a1aa23f6054ad9a3f7b
Starlight Romero <starlightromero@protonmail.com>
2021-05-20 15:37:10 -0700

    [update] remove unused import

--- robinhood/routes.py
+++ robinhood/routes.py
@@ -1,7 +1,6 @@
 """Robinhood routes."""
 from . import app
 from .robinhood import Robinhood
-from flask_api import status
 
 Robinhood.login()
 
@@ -10,10 +9,6 @@ Robinhood.login()
 def search_stocks(query):
     """Search stocks by query."""
     data = Robinhood.search_stocks(query)
-
-    if not data:
-        return "Not found", status.HTTP_400_BAD_REQUEST
-
     return data
 
 
@@ -21,10 +16,6 @@ def search_stocks(query):
 def get_current_price(symbol):
     """Get current price by symbol."""
     data = Robinhood.get_current_price(symbol)
-
-    if not data:
-        return "{} not found".format(symbol), status.HTTP_400_BAD_REQUEST
-
     return data
 
 
@@ -32,10 +23,6 @@ def get_current_price(symbol):
 def get_company_name(symbol):
     """Take a symbol and return the company name as a string."""
     data = Robinhood.get_company_name(symbol)
-
-    if not data:
-        return "{} not found".format(symbol), status.HTTP_400_BAD_REQUEST
-
     return data
 
 
@@ -43,8 +30,4 @@ def get_company_name(symbol):
 def get_historical(symbol, span):
     """Get historial data by symbol and span."""
     historical_data = Robinhood.get_historical(symbol, span)
-
-    if not historical_data:
-        return "{} not found".format(symbol), status.HTTP_400_BAD_REQUEST
-
     return historical_data
