394f195e9da12c0cf006b40db0bfa8f778fab8a6
Starlight Romero <starlightromero@protonmail.com>
2021-06-24 18:07:10 -0700

    [create] wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+from waitress import serve
+from robinhood import app
+import os
+
+if __name__ == "__main__":
+    serve(app, host='0.0.0.0', 
+          port=5000, 
+          threads=8, 
+          url_prefix='/'
+         )
